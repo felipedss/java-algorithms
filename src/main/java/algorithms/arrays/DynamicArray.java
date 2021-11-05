2cf64bcb1920959abe8e783184b2ab8ef84f7335
Felipe da Rosa <felipedss@hotmail.com>
2021-11-05 20:24:42 -0300

    Updating DynamicArray

--- src/main/java/algorithms/arrays/DynamicArray.java
+++ src/main/java/algorithms/arrays/DynamicArray.java
@@ -1,5 +1,7 @@
 package algorithms.arrays;
 
+import static algorithms.arrays.DynamicArray.ResizeType.DECREASE;
+import static algorithms.arrays.DynamicArray.ResizeType.INCREASE;
 import static java.lang.System.arraycopy;
 
 public class DynamicArray {
@@ -15,58 +17,117 @@ public class DynamicArray {
         this.array = new int[DEFAULT_CAPACITY];
     }
 
+    enum ResizeType {
+        INCREASE,
+        DECREASE
+    }
+
+    /**
+     * Create a new Array e copy the elements
+     */
+    private void resize(int newCapacity, ResizeType resizeType) {
+        int[] newArray = new int[newCapacity];
+        if (INCREASE.equals(resizeType)) {
+            arraycopy(this.array, 0, newArray, 0, this.array.length);
+        } else {
+            arraycopy(this.array, 0, newArray, 0, this.current);
+        }
+        this.array = newArray;
+        this.capacity = newCapacity;
+    }
+
+    /**
+     * Size of array
+     */
     public int getSize() {
         return current;
     }
 
+    /**
+     * Determines if array isEmpty
+     */
     public boolean isEmpty() {
         return current == 0;
     }
 
+    /**
+     * Get Capacity of array
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Returns item at given index, blows up if index out of bounds
+     */
     public int at(int position) {
-
         if (position > current) {
             throw new IndexOutOfBoundsException(position);
         }
-
         return array[position];
     }
 
+    /**
+     * inserts item at last position
+     */
     public void push(int val) {
-        allocateArraySize();
+        if (this.current == this.capacity) {
+            this.capacity *= 2;
+            resize(this.capacity, INCREASE);
+        }
         array[current] = val;
         current++;
     }
 
+    /**
+     * inserts item at index, shifts that index's value and
+     * trailing elements to the right
+     */
     public void insert(int index, int val) {
 
         if (index > current) {
             throw new IndexOutOfBoundsException(index);
         }
 
-        allocateArraySize();
+        if (this.current == this.capacity) {
+            this.capacity *= 2;
+            resize(this.capacity, INCREASE);
+        }
 
-        if (current - index >= 0) System.arraycopy(array, index, array, index + 1, current - index);
-
+        System.arraycopy(array, index, array, index + 1, current - index);
         array[index] = val;
         current++;
     }
 
+    /**
+     * can use insert above at index 0
+     */
     public void preprend(int val) {
-        allocateArraySize();
         this.current++;
+        if (this.current == this.capacity) {
+            this.capacity *= 2;
+            resize(this.capacity, INCREASE);
+        }
         arraycopy(this.array, 0, this.array, 1, this.current);
         this.array[0] = val;
     }
 
+    /**
+     * remove from end, return value
+     * <p>
+     * when popping an item, if size is 1/4 of capacity, resize to half
+     */
     public int pop() {
+        if (current == this.capacity / 4) {
+            this.capacity /= 4;
+            resize(this.capacity, DECREASE);
+        }
         return array[--current];
     }
 
+    /**
+     * delete item at index, shifting all trailing elements left
+     */
     public void delete(int index) {
         if (index > current) {
             throw new IndexOutOfBoundsException(index);
@@ -75,31 +136,27 @@ public class DynamicArray {
         this.current--;
     }
 
+    /**
+     * looks for value and removes index holding it (even if in multiple places)
+     */
     public void remove(int val) {
-        for (int i = 0; i <= current; i++) {
-            if (val == this.array[i]) {
-                delete(i);
+        for (int index = 0; index <= current; index++) {
+            if (val == this.array[index]) {
+                delete(index);
             }
         }
     }
 
+    /**
+     * looks for value and returns first index with that value, -1 if not found
+     */
     public int find(int val) {
-        for (int i = 0; i <= current; i++) {
-            if (val == this.array[i]) {
-                return i;
+        for (int index = 0; index <= current; index++) {
+            if (val == this.array[index]) {
+                return index;
             }
         }
         return -1;
     }
-
-    private void allocateArraySize() {
-        if (this.current == this.capacity) {
-            this.capacity *= 2;
-            int[] newArray = new int[this.capacity];
-            arraycopy(this.array, 0, newArray, 0, this.array.length);
-            this.array = newArray;
-        }
-    }
-
 
 }
--- src/test/java/algorithms/arrays/DynamicArrayTest.java
+++ src/test/java/algorithms/arrays/DynamicArrayTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -68,6 +70,17 @@ class DynamicArrayTest {
     }
 
     @Test
+    void testInsertWithResize() {
+        dynamicArray.insert(1, 29);
+        dynamicArray.insert(2, 30);
+        dynamicArray.insert(3, 31);
+        dynamicArray.insert(4, 32);
+        dynamicArray.insert(4, 33);
+        dynamicArray.insert(5, 34);
+        assertEquals(32, dynamicArray.getCapacity());
+    }
+
+    @Test
     void testInsertWithIndexGreaterThanUsed() {
         assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.insert(80, 28));
     }
@@ -78,6 +91,27 @@ class DynamicArrayTest {
         assertEquals(5, dynamicArray.at(0));
         assertEquals(10, dynamicArray.at(1));
         assertEquals(12, dynamicArray.getSize());
+    }
+
+    @Test
+    void testPrependWithResize() {
+        dynamicArray.preprend(29);
+        dynamicArray.preprend(30);
+        dynamicArray.preprend(31);
+        dynamicArray.preprend(32);
+        dynamicArray.preprend(33);
+        dynamicArray.preprend(34);
+        assertEquals(32, dynamicArray.getCapacity());
+    }
+
+    @Test
+    void testPopWithResize() {
+        DynamicArray dynamicArray = new DynamicArray();
+        IntStream.range(0, 32)
+                .forEach(dynamicArray::push);
+        IntStream.range(0, 30)
+                .forEach(v -> dynamicArray.pop());
+        assertEquals(8, dynamicArray.getCapacity());
     }
 
     @Test
